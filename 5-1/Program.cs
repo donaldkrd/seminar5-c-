2917041252928d3061daa15006703752247d8151
Alexey Utkin <alex_habar@mail.ru>
2022-10-05 23:20:00 +0300

    1 и 2 задача

---
+++ 5-1/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размер массива");
+
+int sizeArr = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[sizeArr];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"Количество четных элементов задоного массива: {count}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ 5-2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задайте одномерный массив, заполненный случайными числами (ограничил до 100). Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Clear();
+Console.WriteLine("Введите размер массива");
+int sum = 0;
+
+int sizeArr = int.Parse(Console.ReadLine()!);
+int[] array = new int[sizeArr];
+
+FillArrayRandomarray(array);
+PrintArray(array);
+
+for (int i = 1; i < array.Length; i+=2)
+    sum = sum + array[i];
+    Console.WriteLine($"Cумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomarray(int[] array)
+
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0 , 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
